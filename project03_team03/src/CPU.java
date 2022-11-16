ee92bb47ebc12fafe3a45b951a9cda3ef3929060
heliosyph <byungiselle@gmail.com>
2022-11-15 18:41:15 -0600

    added code to class CPU and TaskThread

--- project03_team03/src/CPU.java
+++ project03_team03/src/CPU.java
@@ -1,2 +1,55 @@
-public class CPU {
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+
+public class CPU implements Runnable{
+    int timeQuantum;
+    int algo;
+    TaskThread currentTask;
+    int id;
+
+    ArrayList<Semaphore> taskThreadSems;
+    ArrayList<Semaphore> dispatchThreadSems;
+    ArrayList<TaskThread> readyQueue;
+
+
+
+   //this will get called from MAIN
+    public CPU(int timeQuantum, int algo, ArrayList<Semaphore> taskThreadSems, ArrayList<Semaphore> dispatchThreadSems, int id,
+               ArrayList<TaskThread> readyQueue) {
+        this.timeQuantum = timeQuantum;
+        this.algo = algo;
+        this.taskThreadSems = taskThreadSems;
+        this.dispatchThreadSems = dispatchThreadSems;
+        this.id = id;
+        this.readyQueue = readyQueue;
+    }
+
+    public void setCurrentTaskID(TaskThread currentTask){
+        this.currentTask = currentTask;
+    }
+
+
+
+
+    @Override
+    public void run() {
+        taskThreadSems.get(currentTask.getId()).acquireUninterruptibly(); //makes sure CPU runs: release thread semaphores
+
+        /**update burst time if RR (in this scenario RR will be assigned to int 3)**/
+        if(algo == 3){
+            //finding burst time per time quantum cycle
+            readyQueue.get(currentTask.getId()).setBurstGoalTime(readyQueue.get(currentTask.getId()).getCurrentBurstTime()+ timeQuantum);
+        }
+        else{
+            //when current burst time = total burst time
+            readyQueue.get(currentTask.getId()).setBurstGoalTime(readyQueue.get(currentTask.getId()).getTotalBurstTime());
+        }
+
+        taskThreadSems.get(currentTask.getId()).release(); //makes sure CPU doesn't start another task while running
+        taskThreadSems.get(currentTask.getId()).acquireUninterruptibly();
+
+
+        dispatchThreadSems.get(id).release();
+    }
 }
--- project03_team03/src/TaskThread.java
+++ project03_team03/src/TaskThread.java
@@ -1,2 +1,61 @@
-public class TaskThread {
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+public class TaskThread implements Runnable {
+    int id;
+    int totalBurstTime;
+    int currentBurstTime;
+    int burstGoalTime;
+    ArrayList<Semaphore> taskThreads;
+
+
+
+    //Constructor is being generated from MAIN
+    public TaskThread(ArrayList<Semaphore> taskThreads, int totalBurstTime, int currentBurstTime, int burstGoalTime, int id){
+        this.taskThreads = taskThreads;
+        this.totalBurstTime = totalBurstTime;
+        this.currentBurstTime = currentBurstTime;
+        this.burstGoalTime = burstGoalTime;
+        this.id = id;
+    }
+
+
+
+    /** getters for other classes to access our objects **/
+    //dispatcher uses this
+    public int getTotalBurstTime() {
+        return totalBurstTime;
+    }
+
+    //CPU uses these
+    public int getCurrentBurstTime() {
+        return currentBurstTime;
+    }
+
+    public void setBurstGoalTime(int burstGoalTime){
+        this.burstGoalTime = burstGoalTime;
+    }
+
+    //CPU uses this
+    public int getBurstGoalTime() {
+        return burstGoalTime;
+    }
+
+    public int getId(){ return id;}
+
+    @Override
+    public void run() {
+        //keeps track of the looping/time for each thread
+        taskThreads.get(id).acquireUninterruptibly();
+
+        while(currentBurstTime < totalBurstTime){
+            taskThreads.get(id).acquireUninterruptibly();
+
+            for(int i = 0; i < burstGoalTime; i++){
+                System.out.println("burst (i): " + i);
+            }
+
+            taskThreads.get(id).release();
+        }
+    }
 }
